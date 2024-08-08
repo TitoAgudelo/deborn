a01afffe5f540784118fcfac9d2e2d06a4426990
Tito Agudelo <titoarturoagudelo@gmail.com>
2024-08-08 17:49:44 -0500

    Deborn send email resend verification

--- next.config.mjs
+++ next.config.mjs
@@ -6,3 +6,12 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+function throwError(envVar) {
+  throw new Error(`Abort: You need to define ${envVar} in the .env file.`);
+}
+
+// Check if essential environment variables are defined
+if (!process.env.RESEND_API_KEY) {
+  throwError("RESEND_API_KEY");
+}
--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -7,17 +7,22 @@ export async function POST(request: NextRequest) {
   try {
     const { firstName, lastName, phone, email, company } = await request.json();
 
-    const response = await resend.emails.send({
+    debugger;
+
+    const { data, error } = await resend.emails.send({
       from: "no_reply@debornconstruction.com",
       to: "titoarturoagudelo@gmail.com",
       subject: "New Contact Form Submission Deborn",
       html: `<p><strong>Name:</strong> ${firstName} ${lastName}</p><p><strong>Email:</strong> ${email}</p><p><strong>Phone:</strong> ${phone}</p><p><strong>Company:</strong> ${company}</p>`,
     });
 
-    return NextResponse.json(
-      { success: true, data: response },
-      { status: 200 }
-    );
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    debugger;
+
+    return NextResponse.json({ success: true, data: data }, { status: 200 });
   } catch (error) {
     console.error("Error sending email:", error);
     return NextResponse.json(
